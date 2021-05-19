867297bf0efa880a670aedcb1a701ff7b1b61d7e
james479 <james.e.glover57@gmail.com>
2021-05-19 01:07:45 -0500

    inital commit

---
+++ MagicNumbers/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using FileSignatures;
+using MagicNumbers.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MagicNumbers.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       public IActionResult GetMagicNumber(IFormFile file)
+        {
+            FileFormat format;
+            var inspector = new FileFormatInspector();
+
+            using (var stream = file.OpenReadStream())
+            {
+                format = inspector.DetermineFileFormat(stream);
+            }
+          
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MagicNumbers/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MagicNumbers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
